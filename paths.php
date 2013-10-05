25a1b5e138288040e8123f139faf48a57403ad8b
Aniket Pant <me@aniketpant.com>
2013-10-05 17:13:28 +0530

    Fix path naming
    Signed-off-by: Aniket Pant <me@aniketpant.com>

--- paths.php
+++ paths.php
@@ -1,9 +1,9 @@
 <?php
 
-define('VENDOR_DIR', './vendor/');
-define('STENCIL_DIR', './stencil/');
+define('VENDOR_DIR', __DIR__.'/vendor/');
+define('STENCIL_DIR', __DIR__.'/stencil/');
 
-define('CONFIG_FILE', './config.json');
+define('CONFIG_FILE', __DIR__.'/config.json');
 
-define('POSTS_DIR', './_posts/');
-define('LAYOUTS_DIR', './_layouts/');
+define('POSTS_DIR', __DIR__.'/_posts/');
+define('LAYOUTS_DIR', __DIR__.'/_layouts/');
